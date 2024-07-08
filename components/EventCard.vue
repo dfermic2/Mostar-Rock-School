b9b2e2db14408db6623b7df60d8c7e91fed9f012
sitnic-a <admir.hhs@gmail.com>
2024-07-08 14:43:16 +0200

    Reusable component for events

---
+++ components/EventCard.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="container p-1">
+    <section class="header p-inline-1 m-minus-1">
+      <div class="location-time-info ml-minus-05">
+        <div class="date">
+          <Icon name="mdi:calendar-check" size="17" />
+          <p class="date-time pl-02">{{ event.date }}</p>
+        </div>
+
+        <div class="time">
+          <Icon class="icon" name="uil:clock" size="17" />
+          <p class="date-time pl-02">{{ event.time }}</p>
+        </div>
+
+        <div class="location">
+          <Icon name="mdi:place" size="17" />
+          <p class="location-place pl-02">{{ event.location }}</p>
+        </div>
+      </div>
+    </section>
+
+    <section class="event-description p-block-1 pl-02">
+      <h2 class="font-weight-900">{{ event.title }}</h2>
+    </section>
+
+    <section class="footer p-inline-1 m-minus-1">
+      <div class="fee-info pl-02">
+        <Icon name="solar:ticker-star-bold" size="17" />
+        <p>
+          <span class="pl-02 font-weight-900">{{ event.fee }}</span>
+        </p>
+      </div>
+
+      <div class="actions mr-05">
+        <button class="btn pl-pr-1 p-block-045">Find out more</button>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script setup>
+const { event } = defineProps(['event'])
+console.log(event)
+</script>
+
+<style scoped>
+.container {
+  width: 330px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  background: #fff;
+  box-shadow: 2px 2px 2px #d6d6d6;
+}
+
+.header {
+  width: 100%;
+  background: #e3e3e3;
+}
+
+.p-inline-1 {
+  padding-inline: 1rem;
+}
+
+.pl-pr-1 {
+  padding-left: 2rem;
+  padding-right: 2rem;
+}
+
+.p-1 {
+  padding: 1rem;
+}
+
+.p-05 {
+  padding: 0.5rem;
+}
+
+.p-block-1 {
+  padding-top: 1.2rem;
+  padding-bottom: 1.2rem;
+}
+
+.p-block-045 {
+  padding-top: 0.45rem;
+  padding-bottom: 0.45rem;
+}
+
+.pl-02 {
+  padding-left: 0.2rem;
+}
+
+.m-minus-1 {
+  margin: -1rem;
+}
+
+.ml-minus-05 {
+  margin-left: -0.5rem;
+}
+
+.mr-05 {
+  margin-right: 0.5rem;
+}
+
+.location-time-info {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+
+.date,
+.time,
+.location {
+  display: flex;
+  align-items: center;
+  font-size: 0.85rem;
+}
+
+.date-time {
+  font-weight: 500;
+}
+
+.location-place {
+  font-weight: 300;
+}
+
+.font-size-11 {
+  font-size: 1.1rem;
+}
+
+.font-weight-900 {
+  font-weight: 900;
+}
+
+.footer {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: #e3e3e3;
+}
+
+.fee-info {
+  display: flex;
+  align-items: center;
+}
+
+.btn {
+  background: #ff0000;
+  background: hsl(0, 240, 120);
+  color: #fff;
+  font-family: Arial, Helvetica, sans-serif;
+  text-transform: uppercase;
+  outline: none;
+  border: none;
+  font-size: 0.65rem;
+  font-weight: 500;
+  border-radius: 0.2rem;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <EventCard :event="events[0]" />
+  </div>
+</template>
+
+<script setup>
+const events = [
+  {
+    id: 1,
+    title: 'Treći po redu MORS session event u K19',
+    date: '27.04.2023',
+    time: '21:00',
+    location: 'MC Pavarotti',
+    fee: '5KM',
+  },
+  {
+    id: 2,
+    title: 'Edo Maajka u Mostaru',
+    date: '27.04.2023',
+    time: '21:00',
+    location: 'OKC Abrasevic',
+    fee: 'Free',
+  },
+]
+</script>
+
+<style lang="scss" scoped></style>
