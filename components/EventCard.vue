dd5e1a8b996659714240c10c0e769770ca548f89
sitnic-a <admir.hhs@gmail.com>
2024-07-19 11:12:42 +0200

    480x255 - design

--- components/EventCard.vue
+++ components/EventCard.vue
@@ -1,40 +1,42 @@
 <template>
-  <div class="container p-1">
-    <section class="header p-inline-1 m-minus-1">
-      <div class="location-time-info ml-minus-05">
-        <div class="date">
-          <Icon name="mdi:calendar-check" size="17" />
-          <p class="date-time pl-05">{{ event.date }}</p>
+  <div>
+    <div class="container">
+      <section class="header p-inline-25 f-size-s">
+        <div class="location-time-info pt-05">
+          <div class="date">
+            <Icon name="mdi:calendar-check" size="17" />
+            <p class="date-time pl-05">{{ event.date }}</p>
+          </div>
+
+          <div class="time">
+            <Icon class="icon" name="uil:clock" size="17" />
+            <p class="f-weight-500 pl-05">{{ event.time }}</p>
+          </div>
+
+          <div class="location">
+            <Icon name="mdi:place" size="17" />
+            <p class="f-weight-300 pl-05">{{ event.location }}</p>
+          </div>
+        </div>
+      </section>
+
+      <section class="event-description p-inline-25">
+        <h2 class="f-size-l">{{ event.title }}</h2>
+      </section>
+
+      <section class="footer p-inline-25">
+        <div class="fee-info">
+          <Icon name="bi:ticket" size="17" />
+          <p class="mr-05 f-weight-600 f-size-m">
+            <span>{{ event.fee }}</span>
+          </p>
         </div>
 
-        <div class="time">
-          <Icon class="icon" name="uil:clock" size="17" />
-          <p class="date-time pl-05">{{ event.time }}</p>
+        <div class="actions">
+          <button class="btn p-btn f-weight-600">Find out more</button>
         </div>
-
-        <div class="location">
-          <Icon name="mdi:place" size="17" />
-          <p class="location-place pl-05">{{ event.location }}</p>
-        </div>
-      </div>
-    </section>
-
-    <section class="event-description p-block-12 pl-05">
-      <h2 class="font-weight-900">{{ event.title }}</h2>
-    </section>
-
-    <section class="footer p-inline-1 m-minus-1">
-      <div class="fee-info pl-05">
-        <Icon name="bi:ticket" size="17" />
-        <p>
-          <span class="pl-05 font-weight-900">{{ event.fee }}</span>
-        </p>
-      </div>
-
-      <div class="actions mr-05">
-        <button class="btn pl-pr-2 p-block-045">Find out more</button>
-      </div>
-    </section>
+      </section>
+    </div>
   </div>
 </template>
 
@@ -45,67 +47,28 @@ console.log(event)
 
 <style scoped>
 .container {
-  width: 330px;
+  width: 480px;
+  height: 255px;
   position: relative;
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
+  justify-content: space-between;
   background: #fff;
   box-shadow: 2px 2px 2px #f1f2f3;
+  box-sizing: border-box;
 }
 
 .header {
   width: 100%;
+  height: 60px;
   background: #f1f5f9;
-}
-
-.p-inline-1 {
-  padding-inline: 1rem;
-}
-
-.pl-pr-2 {
-  padding-left: 2rem;
-  padding-right: 2rem;
-}
-
-.p-1 {
-  padding: 1rem;
-}
-
-.p-05 {
-  padding: 0.5rem;
-}
-
-.p-block-12 {
-  padding-top: 1.2rem;
-  padding-bottom: 1.2rem;
-}
-
-.p-block-045 {
-  padding-top: 0.45rem;
-  padding-bottom: 0.45rem;
-}
-
-.pl-05 {
-  padding-left: 0.5rem;
-}
-
-.m-minus-1 {
-  margin: -1rem;
-}
-
-.ml-minus-05 {
-  margin-left: -0.5rem;
-}
-
-.mr-05 {
-  margin-right: 0.5rem;
+  box-sizing: border-box;
 }
 
 .location-time-info {
   display: flex;
   align-items: center;
-  justify-content: space-around;
+  justify-content: space-between;
 }
 
 .date,
@@ -116,23 +79,8 @@ console.log(event)
   font-size: 0.85rem;
 }
 
-.date-time {
-  font-weight: 500;
-}
-
-.location-place {
-  font-weight: 300;
-}
-
-.font-size-11 {
-  font-size: 1.1rem;
-}
-
-.font-weight-900 {
-  font-weight: 900;
-}
-
 .footer {
+  height: 70px;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -140,7 +88,9 @@ console.log(event)
 }
 
 .fee-info {
+  width: 81px;
   display: flex;
+  justify-content: space-around;
   align-items: center;
 }
 
@@ -155,5 +105,49 @@ console.log(event)
   font-size: 0.65rem;
   font-weight: 500;
   border-radius: 0.2rem;
+}
+
+.p-btn {
+  padding: 0.56rem 2rem 0.5rem 2.0625rem;
+}
+
+.p-inline-25 {
+  padding-inline: 2.5rem;
+}
+
+.pt-05 {
+  padding-top: 0.5rem;
+}
+
+.pl-05 {
+  padding-left: 0.5rem;
+}
+
+.mr-05 {
+  margin-right: 0.5rem;
+}
+
+.f-size-s {
+  font-size: 0.9375rem;
+}
+
+.f-size-m {
+  font-size: 1.25rem;
+}
+
+.f-size-l {
+  font-size: 1.5625rem;
+}
+
+.f-weight-300 {
+  font-weight: 300;
+}
+
+.f-weight-500 {
+  font-weight: 500;
+}
+
+.f-weight-600 {
+  font-weight: 600;
 }
 </style>
