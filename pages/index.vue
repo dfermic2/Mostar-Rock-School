679178b900cd0f26d848c2e7bd28db61dd644957
sitnic-a <admir.hhs@gmail.com>
2024-07-29 18:01:08 +0200

    Reusable component contribution types

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,47 @@
 <template>
   <div class="container">
-    <EventCard :event="events[0]" />
-
-    <MediaLinks :mediaPosition="'side'" />
+    <div style="margin: 20px">
+      <Contributions :contributionTypes="contributionTypes" />
+    </div>
   </div>
 </template>
 
 <script setup>
-import BasicSponsorCard from '~/components/BasicSponsorCard.vue'
-import GoldenSponsorCard from '~/components/GoldenSponsorCard.vue'
+import tongue from '../assets/images/tongue.png'
+import bubbles from '../assets/images/bubbles.png'
+import drum from '../assets/images/drum.png'
+import diamond from '../assets/images/diamond.png'
 
+let contributionTypes = [
+  {
+    id: 1,
+    type: 'Education',
+    content: `By funding our classrooms, you provide quality music education to talented individuals who may not have access otherwise. Your contribution helps us offer comprehensive programs, individual lessons, group sessions, band collaborations, and live performances.`,
+    icon: tongue,
+    iconName: 'Tongue',
+  },
+  {
+    id: 2,
+    type: 'Community Integration',
+    content: `Your donation allows us to conduct workshops and outreach programs, reaching out to marginalized communities and promoting inclusivity. Through music, we can bridge divides and foster understanding among diverse groups.`,
+    icon: bubbles,
+    iconName: 'Bubbles',
+  },
+  {
+    id: 3,
+    type: 'Impactful events',
+    content: `By supporting our concerts, festivals, and special events, you help us create memorable experiences for our students and the wider community. These events not only showcase the incredible talent nurtured at the Mostar Rock School but also bring people together in celebration of music.`,
+    icon: drum,
+    iconName: 'Drum',
+  },
+  {
+    id: 4,
+    type: 'Scholarship',
+    content: `Your generosity can provide scholarships and financial aid packages to deserving students. With your support, we ensure that talented individuals, regardless of their financial circumstances, have the opportunity to pursue their musical dreams and unlock their full potential.`,
+    icon: diamond,
+    iconName: 'Diamond',
+  },
+]
 const events = [
   {
     id: 1,
