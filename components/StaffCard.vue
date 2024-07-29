609bece5c46dab56b5f9b95af9efec8258bde7d3
sitnic-a <admir.hhs@gmail.com>
2024-07-29 14:06:51 +0200

    Reusable component for staff members

---
+++ components/StaffCard.vue
@@ -0,0 +1,182 @@
+<template>
+  <div>
+    <div class="container" role="staff-container">
+      <section class="staff-container">
+        <div
+          class="shape"
+          :class="{
+            circle: person.shape === 'circle',
+            rectangle: person.shape === 'rectangle',
+            triangle: person.shape === 'triangle',
+            'half-rectangle': person.shape === 'half-rectangle',
+            leaf: person.shape === 'leaf',
+          }"
+        ></div>
+        <img
+          :src="person.img"
+          type="image/webp"
+          :alt="`${person.name} photo`"
+        />
+        <div class="staff-info-container">
+          <p>{{ person.name }}</p>
+          <p>{{ person.department }}</p>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script setup>
+let { person } = defineProps(['person'])
+console.log('Person -> ', person)
+
+onMounted(() => {
+  let container = document.querySelector('.container[role="staff-container"]')
+  let staffInfoContainer = document.querySelector('.staff-info-container')
+  let shape = document.querySelector('.shape')
+  let img = document.querySelector('img')
+
+  container.addEventListener('mouseover', () => {
+    //image
+    img.style.transform = 'scale(1.05)'
+    img.style.clipPath = 'rect(0% 100% 97.5% 0%)'
+    img.style.transition = '500ms'
+    //shape
+    if (shape.classList.contains('circle')) {
+      let circle = document.querySelector('.circle')
+      circle.style.background = '#ff0000'
+      circle.style.transition = '500ms'
+    } else if (shape.classList.contains('rectangle')) {
+      let rectangle = document.querySelector('.rectangle')
+      rectangle.style.background = '#ff0000'
+      rectangle.style.transition = '500ms'
+    } else if (shape.classList.contains('triangle')) {
+      let triangle = document.querySelector('.triangle')
+      triangle.style.borderTop = '280px solid #ff0000'
+      triangle.style.transition = '500ms'
+    } else if (shape.classList.contains('half-rectangle')) {
+      let halfRectangle = document.querySelector('.half-rectangle')
+      halfRectangle.style.borderTop = '0px solid #ff0000'
+      halfRectangle.style.borderBottom = '280px solid #ff0000'
+      halfRectangle.style.transition = '500ms'
+    } else if (shape.classList.contains('leaf')) {
+      let leaf = document.querySelector('.leaf')
+      leaf.style.borderLeft = '280px solid #ff0000'
+      leaf.style.transition = '500ms'
+    }
+    //staff-info-container
+    staffInfoContainer.style.color = '#ff0000'
+    staffInfoContainer.style.transition = '500ms'
+  })
+
+  container.addEventListener('mouseleave', () => {
+    //image
+    img.style.transform = 'scale(1)'
+    img.style.clipPath = 'rect(0% 100% 100% 0%)'
+    img.style.transition = '500ms'
+    // shape
+    if (shape.classList.contains('circle')) {
+      let circle = document.querySelector('.circle')
+      circle.style.background = '#000'
+      circle.style.transition = '500ms'
+    } else if (shape.classList.contains('rectangle')) {
+      let rectangle = document.querySelector('.rectangle')
+      rectangle.style.background = '#000'
+      rectangle.style.transition = '500ms'
+    } else if (shape.classList.contains('triangle')) {
+      let triangle = document.querySelector('.triangle')
+      triangle.style.borderTop = '280px solid #000'
+      triangle.style.transition = '500ms'
+    } else if (shape.classList.contains('half-rectangle')) {
+      let halfRectangle = document.querySelector('.half-rectangle')
+      halfRectangle.style.borderTop = '0px solid #000'
+      halfRectangle.style.borderBottom = '280px solid #000'
+      halfRectangle.style.transition = '500ms'
+    } else if (shape.classList.contains('leaf')) {
+      let leaf = document.querySelector('.leaf')
+      leaf.style.borderLeft = '280px solid #000'
+      leaf.style.transition = '500ms'
+    }
+    //staff-info-container
+    staffInfoContainer.style.color = '#000'
+    staffInfoContainer.style.transition = '500ms'
+  })
+})
+</script>
+
+<style scoped>
+.container {
+  width: 17.5rem;
+  height: 22.5rem;
+  position: relative;
+}
+
+.staff-container {
+  overflow: hidden;
+}
+
+.shape {
+  width: 17.5rem;
+  height: 17.5rem;
+  position: absolute;
+  top: 0;
+  z-index: 0;
+}
+
+.circle {
+  background: #000;
+  border-radius: 50%;
+  clip-path: rect(0% 100% 92.5% 0%);
+}
+
+.rectangle {
+  background: #000;
+  border-radius: 0;
+  clip-path: rect(0% 100% 92.5% 0%);
+}
+
+.triangle {
+  border-left: 8.75rem solid transparent;
+  border-right: 8.75rem solid transparent;
+  border-top: 17.5rem solid #000;
+  clip-path: rect(0% 100% 92.5% 0%);
+}
+
+.half-rectangle {
+  border-left: 0px solid transparent;
+  border-right: 17.5rem solid transparent;
+  border-top: 0px solid #000;
+  border-bottom: 17.5rem solid #000;
+  clip-path: rect(0% 100% 92.5% 0%);
+}
+
+.leaf {
+  border-top-left-radius: 8.75rem;
+  border-bottom-right-radius: 8.75rem;
+  border-left: 17.5rem solid #000;
+  border-bottom: 0px solid transparent;
+  clip-path: rect(0% 100% 92.5% 0%);
+}
+
+.staff-info-container {
+  margin-top: -0.625rem;
+  text-align: center;
+}
+
+.staff-info-container p:first-child {
+  font-size: 1.1875rem;
+  margin-top: 0.625rem;
+}
+
+.staff-info-container p:nth-child(2) {
+  font-size: 0.875rem;
+}
+
+img {
+  position: relative;
+  top: -1.25rem;
+  z-index: 1;
+  width: 17.5rem;
+  height: 17.5rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,112 @@
 <template>
   <div class="container">
-    <EventCard :event="events[0]" />
-
-    <MediaLinks :mediaPosition="'side'" />
+    <div style="margin-left: 20px; margin-top: 20px">
+      <StaffCard :person="staff.lecturers[0]" />
+    </div>
   </div>
 </template>
 
 <script setup>
-import BasicSponsorCard from '~/components/BasicSponsorCard.vue'
-import GoldenSponsorCard from '~/components/GoldenSponsorCard.vue'
+//Management and Administration images
+import MarinBosnjak from '../assets/images/staff/marin-bosnjak.webp'
+import JasminaKazazic from '../assets/images/staff/jasmina-kazazic.webp'
+import LubnaCuric from '../assets/images/staff/lubna-curic.webp'
+import OrhanMaslo from '../assets/images/staff/orhan-maslo.webp'
+//Programme images
+import DjenoMujic from '../assets/images/staff/djeno-mujic.webp'
+import MonikaGrubisic from '../assets/images/staff/monika-grubisic.webp'
+import NerminaCoric from '../assets/images/staff/nermica-coric.webp'
+//Lecturers images
+import AtilaAksoj from '../assets/images/staff/atilla-aksoj.webp'
+import IvanKovacevic from '../assets/images/staff/ivan-kovacevic.webp'
+import DomagojZelenika from '../assets/images/staff/domagoj-zelenika.webp'
+import SeminMerzic from '../assets/images/staff/semin-merzic.webp'
+
+const staff = {
+  managementAndAdministration: [
+    {
+      id: 1,
+      name: 'Marin Bošnjak',
+      department: 'Head of Production',
+      shape: 'circle',
+      img: MarinBosnjak,
+    },
+    {
+      id: 2,
+      name: 'Jasmina Kazazić',
+      department: 'Financial Manager',
+      shape: 'rectangle',
+      img: JasminaKazazic,
+    },
+    {
+      id: 3,
+      name: 'Lubna Ćurić',
+      department: 'Project Assistant/Booking Manager',
+      shape: 'triangle',
+      img: LubnaCuric,
+    },
+    {
+      id: 4,
+      name: 'Orhan Maslo',
+      department: 'Project Manager',
+      shape: 'circle',
+      img: OrhanMaslo,
+    },
+  ],
+  programme: [
+    {
+      id: 1,
+      name: 'Đeno Mujić',
+      department: 'Creative Department/Drums & Audio Production',
+      shape: 'triangle',
+      img: DjenoMujic,
+    },
+    {
+      id: 2,
+      name: 'Monika Grubišić-Ćabo',
+      department: 'Head of Education/Vocal Coach',
+      shape: 'circle',
+      img: MonikaGrubisic,
+    },
+    {
+      id: 3,
+      name: 'Nermina Ćorić',
+      department: 'Student Administration Officer',
+      shape: 'rectangle',
+      img: NerminaCoric,
+    },
+  ],
+  lecturers: [
+    {
+      id: 1,
+      name: 'Atila Aksoj',
+      department: 'Guitar',
+      shape: 'half-rectangle',
+      img: AtilaAksoj,
+    },
+    {
+      id: 2,
+      name: 'Ivan Kovacevic',
+      department: 'Guitar',
+      shape: 'leaf',
+      img: IvanKovacevic,
+    },
+    {
+      id: 3,
+      name: 'Domagoj Zelenika',
+      department: 'Guitar',
+      shape: 'triangle',
+      img: DomagojZelenika,
+    },
+    {
+      id: 4,
+      name: 'Semin Merzić',
+      department: 'Band Coach',
+      shape: 'circle',
+      img: SeminMerzic,
+    },
+  ],
+}
 
 const events = [
   {
@@ -30,4 +128,4 @@ const events = [
 ]
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
