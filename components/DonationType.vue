3148b9a3e79e70dd775db396f236c155fa456824
dfermic2 <dfermic2@etf.unsa.ba>
2024-08-01 15:41:02 +0200

    Made list of donation types

---
+++ components/DonationType.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="donation-type">
+    <div class="container">
+      <div class="content">
+        <h1>{{ donationType.title }}</h1>
+        <p>{{ donationType.description }}</p>
+        <button>
+          <Icon
+            v-if="`${donationType.btnIcn}`"
+            :name="`${donationType.btnIcn}`"
+            size="1rem"
+          />
+          {{ donationType.btnText }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { donationType } = defineProps(["donationType"]);
+</script>
+
+<style scoped>
+.donation-type {
+  width: 100%;
+}
+.container {
+  display: block;
+  max-width: 36.25rem;
+  min-height: 16.25rem;
+  border: 1px solid red;
+  border-radius: 0.4rem;
+  padding: 1.875rem 1.375rem;
+  box-sizing: border-box;
+}
+.content {
+  color: white;
+  max-width: 31.25rem;
+  min-height: 13.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-inline: auto;
+  margin-top: 0.625rem;
+  box-sizing: border-box;
+}
+
+button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.7rem;
+  max-width: 18.75rem;
+  height: 2.8rem;
+  border-radius: 0.25rem;
+  border: none;
+  background: red;
+  color: white;
+  font-size: 0.9rem;
+  letter-spacing: 0.02rem;
+  font-weight: bold;
+  box-shadow: 2px 2px 3px 0px rgba(0, 0, 0, 0.2);
+  transition: background 0.2s ease-in-out;
+}
+
+button:hover {
+  cursor: pointer;
+  background: #b00;
+}
+
+h1 {
+  padding: 0;
+  margin: 0;
+  font-family: "F37Hybrid-Bold";
+  font-size: 1.25rem;
+}
+
+p {
+  font-size: 0.94rem;
+  line-height: 1.45rem;
+}
+</style>
--- pages/support/index.vue
+++ pages/support/index.vue
@@ -1,5 +1,42 @@
 <template>
-  <div>
-    <p>Support</p>
+  <div style="padding: 4rem; background: #222">
+    <DonationTypes :donationTypes="donationTypes" />
   </div>
 </template>
+
+<script setup>
+const donationTypes = [
+  {
+    id: 1,
+    title: "Online donation",
+    description:
+      "Visit our secure online donation platform to contribute easily and securely. Choose a one-time donation or become a monthly supporter, providing sustained support for our programs.",
+    btnIcn: "ion:heart",
+    btnText: "DONATE NOW!",
+  },
+  {
+    id: 2,
+    title: "Corporate Sponsorship",
+    description:
+      "Explore our corporate sponsorship opportunities and make a lasting impact as a business. Together, we can build stronger communities through the power of music.",
+    btnIcn: "",
+    btnText: "CONTACT US",
+  },
+  {
+    id: 3,
+    title: "In-Kind Donations",
+    description:
+      "If you have musical instruments, equipment, or other resources that can benefit our programs, consider making an in-kind donation. Your contribution directly supports the growth and development of our students.",
+    btnIcn: "",
+    btnText: "GET IN TOUCH",
+  },
+  {
+    id: 4,
+    title: "Fundraising Events",
+    description:
+      "Get involved in our fundraising events or organize your own to support the Mostar Rock School. From benefit concerts to charity auctions, there are various ways to rally support and make a meaningful impact.",
+    btnIcn: "",
+    btnText: "LET'S TALK ABOUT YOUR IDEAS",
+  },
+];
+</script>
