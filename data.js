d4dffbe58e2eb59e31dcf20dc29e866246cb4fb9
sitnic-a <admir.hhs@gmail.com>
2024-08-08 20:12:07 +0200

    Support donors page

--- data.js
+++ data.js
@@ -192,3 +192,128 @@ export const staff = {
     },
   ],
 }
+
+import norway_embassy_sarajevo from '../public/assets/images/Norwegian Embassy Sarajevo.png'
+import kingdom_of_netherland from '../public/assets/images/Kingdom of Netherland.png'
+import njemacka_ambasada_sarajevo from '../public/assets/images/Njemacka ambasada Sarajevo.png'
+import embassy_of_sweden_sarajevo from '../public/assets/images/Embassy of Sweden Sarajevo.png'
+import swiss_agency from '../public/assets/images/SWISS-AGENCY.png'
+
+export const platinumSponsors = [
+  {
+    id: 1,
+    name: 'Norwegian Embassy Sarajevo',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta exercitationem dolore accusamus at et aperiam, harum minus. Minima, expedita delectus.',
+    img: norway_embassy_sarajevo,
+  },
+  {
+    id: 2,
+    name: 'Kingdom of the Netherlands',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta exercitationem dolore accusamus at et aperiam, harum minus. Minima, expedita delectus.',
+    img: kingdom_of_netherland,
+  },
+]
+
+import federalno_ministarstvo_kulture_i_sporta from '../public/assets/images/FBIH-Kultura i sport.png'
+import usaid from '../public/assets/images/USAID.png'
+import grad_mostar from '../public/assets/images/GRAD-MOSTAR 242x121.png'
+import amus from '../public/assets/images/AMUS.png'
+import spark from '../public/assets/images/SPARK.png'
+import federalno_ministarstvo_obrazovanja_i_nauke from '../public/assets/images/Federalno ministarstvo obrazovanja i nauke.png'
+import usa_embassy_sarajevo from '../public/assets/images/USA Embassy Sarajevo.png'
+import vlada_hnk from '../public/assets/images/VLADA-HNK.png'
+import stability_pact_for_eastern_europe from '../public/assets/images/STABILITY-PAKT.png'
+import dubioza_kolektiv from '../public/assets/images/DUBIOZA KOLEKTIV.png'
+import red_bull from '../public/assets/images/RED-BULL.png'
+import osce from '../public/assets/images/OSCE.png'
+import robert_bosch_stiftung from '../public/assets/images/ROBERT-BOSCH.png'
+
+export const basicSponsors = [
+  {
+    id: 1,
+    name: 'Kingdom of the Netherlands',
+    img: kingdom_of_netherland,
+  },
+  {
+    id: 2,
+    name: 'Norwegian Embassy Sarajevo',
+    img: norway_embassy_sarajevo,
+  },
+  {
+    id: 3,
+    name: 'Federalno ministarstvo kulture i sporta',
+    img: federalno_ministarstvo_kulture_i_sporta,
+  },
+  {
+    id: 4,
+    name: 'USAID',
+    img: usaid,
+  },
+  {
+    id: 5,
+    name: 'Embassy of Sweden Sarajevo',
+    img: embassy_of_sweden_sarajevo,
+  },
+  {
+    id: 6,
+    name: 'Grad Mostar',
+    img: grad_mostar,
+  },
+  {
+    id: 7,
+    name: 'Swiss Agency for Development and Cooperation',
+    img: swiss_agency,
+  },
+  {
+    id: 8,
+    name: 'AMUS',
+    img: amus,
+  },
+  {
+    id: 9,
+    name: 'SPARK',
+    img: spark,
+  },
+  {
+    id: 10,
+    name: 'Federalno ministarstvo obrazovanja i nauke',
+    img: federalno_ministarstvo_obrazovanja_i_nauke,
+  },
+  {
+    id: 11,
+    name: 'USA Embassy Sarajevo',
+    img: usa_embassy_sarajevo,
+  },
+  {
+    id: 12,
+    name: 'Vlada HNK',
+    img: vlada_hnk,
+  },
+  {
+    id: 13,
+    name: 'Stability Pack',
+    img: stability_pact_for_eastern_europe,
+  },
+  {
+    id: 14,
+    name: 'Dubioza Kolektiv',
+    img: dubioza_kolektiv,
+  },
+  {
+    id: 15,
+    name: 'Red Bull',
+    img: red_bull,
+  },
+  {
+    id: 16,
+    name: 'OSCE',
+    img: osce,
+  },
+  {
+    id: 17,
+    name: 'Robert Bosch Stiftung',
+    img: robert_bosch_stiftung,
+  },
+]
--- pages/support/donors.vue
+++ pages/support/donors.vue
@@ -1,10 +1,164 @@
 <template>
   <div>
-    <div class="bg-grey">
+    <div class="container">
       <header>
         <Navbar />
+        <div class="hero">
+          <div class="info">
+            <div class="about">
+              <p @click="navigateTo(`/support`)">< Support</p>
+              <h1>No man is an island…</h1>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Dignissimos omnis optio architecto laudantium eveniet.
+                Voluptates, consequatur accusantium iusto omnis aperiam
+                reprehenderit fugiat quasi neque! Id, corporis totam adipisci
+                deleniti officia quos, nobis nostrum autem optio ad tenetur
+                explicabo ea fuga!
+              </p>
+            </div>
+            <div class="donations">
+              <DonationForm />
+            </div>
+          </div>
+        </div>
       </header>
+
+      <section class="sponsors">
+        <div class="sponsors-content">
+          <h1>Platinum Sponsors</h1>
+          <div class="platinum-sponsors-container">
+            <div v-for="platinum in platinumSponsors" :key="platinum.id">
+              <PlatinumSponsorCard :sponsor="platinum" />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="donors-throughout-years">
+        <div class="sponsors-content">
+          <h1>Our supporters throughout the years</h1>
+          <div class="basic-sponsors-container">
+            <div v-for="basic in basicSponsors" :key="basic.id">
+              <BasicSponsorCard :sponsor="basic" />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <Donation />
     </div>
-    <p>Our Donors</p>
   </div>
 </template>
+
+<script setup>
+import { basicSponsors, platinumSponsors } from '~/data'
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  max-width: 100vw;
+}
+
+header {
+  background: linear-gradient(
+      0deg,
+      rgba(0, 0, 0, 1) 0%,
+      rgba(9, 9, 121, 0) 33%,
+      rgba(7, 7, 97, 0) 67%,
+      rgba(0, 0, 0, 1) 100%
+    ),
+    url('../../public/assets/images/support-donors/support-donors-header-cover-photo.png');
+  background-size: cover;
+}
+
+.hero {
+  margin-top: 12.1875rem;
+
+  margin-bottom: 6.25rem;
+  min-height: 53.125rem;
+  max-width: 74rem;
+  margin-inline: auto;
+}
+
+.info {
+  height: 30.3125rem;
+  display: flex;
+  align-items: center;
+  gap: 6.25rem;
+  color: #fff;
+}
+
+.info > .about {
+  max-width: 40.625rem;
+  height: 19.875rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+}
+
+.about > p:first-child {
+  cursor: pointer;
+  margin-bottom: 1.5625rem;
+  font-size: 1.875rem;
+  font-family: 'F37Hybrid-Bold';
+}
+
+.about > p:first-child:hover {
+  color: #ff0000;
+  transition: 500ms;
+}
+
+.about > p:first-child:not(hover) {
+  color: #fff;
+  transition: 500ms;
+}
+
+.about > h1 {
+  margin-bottom: 0.9375rem;
+  font-size: 4.6875rem;
+  font-family: 'F37Hybrid-Bold';
+}
+
+.about > p:last-child {
+  font-size: 1rem;
+  line-height: 1.4375rem;
+}
+
+.sponsors {
+  max-width: 100vw;
+  margin-bottom: 12.5rem;
+}
+
+.sponsors-content {
+  max-width: 74rem;
+  margin-inline: auto;
+}
+
+.sponsors-content > h1 {
+  font-size: 1.875rem;
+  font-family: 'F37Hybrid-Bold';
+}
+
+.platinum-sponsors-container {
+  display: flex;
+  gap: 1.25rem;
+  margin-top: 2.4375rem;
+}
+
+.donors-throughout-years {
+  margin-bottom: 6.25rem;
+}
+
+.basic-sponsors-container {
+  margin-top: 2.4375;
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  gap: 1.25rem;
+}
+</style>
