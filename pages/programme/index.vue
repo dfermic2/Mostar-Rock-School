251baf46f4e35bdf3cc87b618047d942e911a7b5
sitnic-a <admir.hhs@gmail.com>
2024-08-09 02:55:56 +0200

    Worst page ever - to refactor

--- pages/programme/index.vue
+++ pages/programme/index.vue
@@ -1,10 +1,539 @@
 <template>
   <div>
-    <div class="bg-grey">
+    <div class="container">
       <header>
         <Navbar />
+        <div class="hero">
+          <div class="info">
+            <h1>School programme</h1>
+            <p>
+              Mostar Rock School offers a comprehensive programme that
+              encompasses everything young musicians might need when starting
+              their musical journey. From educational activities in both
+              individual and group formats, to production of events, creative
+              development, studio recordings, publishing and booking.
+            </p>
+            <button type="button">Enroll now</button>
+          </div>
+          <div class="drums-container">
+            <div class="drumms">
+              <img
+                src="../../public/assets/images/programme/drums.png"
+                alt="Drums photo"
+                title="Drums photo"
+              />
+            </div>
+          </div>
+        </div>
       </header>
+      <section class="music-lessons">
+        <div class="articles">
+          <h2>Music Lessons</h2>
+          <div class="article-text">
+            <strong>
+              <p>
+                The heart of Rock School Mostar is an educational/teaching
+                program that has been held cyclically since 2012 from the
+                beginning of September to the beginning of June and which in
+                recent years has been attended by between 200 and 250
+                participants per year. The students of Rock School Mostar come
+                from 10 countries around the world.
+              </p>
+            </strong>
+          </div>
+          <div class="article-text">
+            <p>
+              In addition to instruments/departments, the teaching program is
+              also divided into individual and group teaching, where each
+              lecturer creates an author's curriculum and program for both types
+              of teaching adapted to each student and group. Classes are based
+              on improving technical playing, rhythmicity, harmonization, music
+              theory, and the first steps of pairing different
+              instruments/vocals, which is a prerequisite for playing in a band
+              or orchestra. The eight rooms of the Rock School Mostar are fully
+              equipped with all the necessary means for musical education and
+              creative work. Instruments, loudspeaker, computer, Smart TV. Some
+              of the rooms are specified only for certain classes, e.g. lessons
+              for vocals, guitar, keyboards... Rock school Mostar implements
+              over 8900 hours of teaching program and over 3000 hours of
+              independent and group exercises during one school year.
+            </p>
+          </div>
+          <button class="btn">Ready to rock with us?</button>
+        </div>
+
+        <div class="curriculum-offer">
+          <h3>The curriculum offers education for:</h3>
+          <div class="offers">
+            <div class="offer">
+              <div class="box"></div>
+              <p>Drums</p>
+            </div>
+
+            <div class="offer">
+              <div class="box"></div>
+              <p>Bass Guitar</p>
+            </div>
+
+            <div class="offer">
+              <div class="box"></div>
+              <p>Electric Music Production</p>
+            </div>
+
+            <div class="offer">
+              <div class="box"></div>
+              <p>Keyboards (electric piano)</p>
+            </div>
+
+            <div class="offer">
+              <div class="box"></div>
+              <p>Electric guitar</p>
+            </div>
+
+            <div class="offer">
+              <div class="box"></div>
+              <p>Workshops and masterclasses</p>
+            </div>
+
+            <div class="offer">
+              <div class="box"></div>
+              <p>Vocals</p>
+            </div>
+
+            <div class="offer">
+              <div class="box"></div>
+              <p>Audio recording production</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="band-img">
+        <iframe
+          width="1180"
+          height="650"
+          src="https://www.youtube.com/embed/6siKtbEkNxs?si=NtgfUzt8tXyyMD9i"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </section>
+
+      <section class="session-band-program-background">
+        <section class="session-band-program">
+          <h2>Session Band Program</h2>
+          <div class="session-band-program-container">
+            <div class="article-texts">
+              <strong>
+                <p>
+                  During the school year, Rock School Mostar forms over 70 band
+                  formations with over 150 participants gets the opportunity to
+                  be a member of the band and through cooperation and creativity
+                  build character, knowledge and experience. These bands are
+                  what we call "classes" in classical education.
+                </p>
+              </strong>
+              <p>
+                What makes this program unique is the mixing of band members
+                through 5 cycles a year, where each the formation has 40 days to
+                meet, exchange knowledge and experience, research a certain
+                musical decade or genre, and creating at least one author's
+                work. The crown of every cycle of the Session band program is a
+                public concert, which is organised 5 times a year in cooperation
+                with partner organisations / institutions in Mostar. At every
+                program concert, which is free for visitors, 10-15 band
+                formations are presented.
+              </p>
+              <p>Attendance at program concerts is 1800+ visitors per year.</p>
+              <p>
+                The bands that have the most interesting musical creations,
+                according to the lecturer's evaluations, record their works
+                studio, and therefore go through education in the field of audio
+                production. The recordings become an eternal bond between these
+                young people, while social networks and local radio stations
+                provide opportunities for songs reach a wider audience. During
+                one school year, more than 1000 hours of sessions of the program
+                are held.
+              </p>
+              <button class="btn" type="button">Ready to rock with us?</button>
+            </div>
+            <div class="session-band-photo-container">
+              <img
+                src="../../public/assets/images/programme/black-white-band-playing.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </section>
+      </section>
+
+      <section class="music-camps">
+        <div class="info">
+          <h2>Music Camps</h2>
+          <p>
+            Rock School Mostar has so far taken part in exchanges and music
+            camps in Macedonia, Croatia, Slovenia, France, Germany, Serbia,
+            Austria.
+          </p>
+          <p>
+            The school also implemented two music camps in its own organization.
+            The first camp was held in the Roma settlement of Kiseljak near
+            Tuzla in 2015 for 60 participants from 4 BH cities, and the second
+            in the Olympic Hotel Bistrica in Jahorina in 2016 for 70
+            participants from 18 BH cities.
+          </p>
+          <button class="btn" type="button">Get in touch to learn more</button>
+        </div>
+        <div class="photo-container">
+          <iframe
+            width="440"
+            height="250"
+            src="https://www.youtube.com/embed/ythCtQHOnrU?si=CiaD7nYXFbBVu-em"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </section>
+
+      <section class="event-festival">
+        <div class="event-container">
+          <div class="left-side">
+            <img
+              src="../../public/assets/images/programme/guy-with-guitar.png"
+              alt=""
+            />
+          </div>
+
+          <div class="right-side">
+            <h2>Events and festivals</h2>
+            <p>
+              Depending on the situation or need, the production department also
+              functions as an organizer, promoter, technical producer,
+              coordinator, and/or executive producer.
+            </p>
+            <p>
+              MoRS started and so far realized two editions of the festival Open
+              City Mostar, four editions of Old Town Street Fest festival that
+              takes place as part of the Red Bull Cliff Diving acrobatic world
+              competition jumps from Stari Most in Mostar, two editions of the
+              Winterville Festival, the Dubioze Kolektiv Concert at SC
+              Kantarevac stadium. MoRS also organizes a concert every year for
+              World Music Day on June 21, when he proudly presents the young
+              talents of his program and invites a regional guest.
+            </p>
+            <button class="btn">Get in touch to learn more</button>
+          </div>
+        </div>
+      </section>
+
+      <section class="spread-the-word">
+        <SpreadTheWord />
+      </section>
     </div>
-    <p>Programme</p>
   </div>
 </template>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  max-width: 100vw;
+  /* background: yellow; */
+}
+
+header {
+  background: linear-gradient(
+      0deg,
+      rgba(0, 0, 0, 1) 0%,
+      rgba(9, 9, 121, 0) 33%,
+      rgba(7, 7, 97, 0) 67%,
+      rgba(0, 0, 0, 1) 100%
+    ),
+    url('../../public/assets/images/programme/programme-header-cover-photo.png');
+  background-size: cover;
+  height: 1030px;
+  max-width: 100vw;
+  margin-bottom: 180px;
+}
+
+.hero {
+  margin-top: 400px;
+  max-width: 74rem;
+  height: 660px;
+  margin-inline: auto;
+  display: flex;
+}
+
+.info {
+  max-width: 773px;
+  height: 660px;
+  display: flex;
+  flex-direction: column;
+  color: #fff;
+}
+
+.info > h1 {
+  font-size: 75px;
+  line-height: 85px;
+  font-family: 'F37Hybrid-Bold';
+  margin-bottom: 35px;
+}
+
+.info > p {
+  font-size: 16px;
+  line-height: 23px;
+  margin-bottom: 40px;
+}
+
+.info > button {
+  width: 220px;
+  height: 45px;
+  padding: 14px 61.5px 13px 62.5px;
+  border-radius: 3px;
+  outline: none;
+  border: none;
+  background: #ff0000;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: 600;
+  cursor: pointer;
+}
+
+.info > button:hover {
+  background: #b00;
+  transition: 500ms;
+}
+
+.info > button:not(hover) {
+  background: #ff0000;
+  transition: 500ms;
+}
+
+.drums-container {
+  height: 660px;
+  margin-left: 250px;
+  display: grid;
+}
+
+.drumms {
+  display: flex;
+  flex-direction: column-reverse;
+  width: 670px;
+}
+
+.music-lessons {
+  max-width: 74rem;
+  margin-inline: auto;
+  display: flex;
+  align-items: end;
+  gap: 60px;
+}
+
+.articles {
+  margin-top: 30px;
+  max-width: 680px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+.articles > h2 {
+  font-size: 40px;
+  font-family: 'F37Hybrid-Bold';
+}
+
+.article-text {
+  text-align: justify;
+}
+
+.btn {
+  width: fit-content;
+  height: 45px;
+  margin-top: 10px;
+  padding: 14px 43.5px 13px 44.5px;
+  border: none;
+  outline: none;
+  border-radius: 3px;
+  background: #ff0000;
+  color: #fff;
+  text-transform: uppercase;
+  font-size: 14px;
+  font-weight: 500;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #b00;
+  transition: 500ms;
+}
+
+.btn:not(hover) {
+  background: #ff0000;
+  transition: 500ms;
+}
+
+.curriculum-offer {
+  max-width: 440px;
+  padding: 40px 20px 40px 40px;
+  background: #fafbfb;
+  border-radius: 5px;
+  box-shadow: 1px 4px 6px #cbcbcb;
+  margin-bottom: 50px;
+}
+
+.curriculum-offer > h3 {
+  font-size: 19px;
+  margin-bottom: 20px;
+  line-height: 25px;
+}
+
+.offers {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.offer {
+  display: flex;
+  align-items: center;
+  gap: 30px;
+}
+
+.box {
+  width: 10px;
+  height: 10px;
+  background: #ff0000;
+}
+
+.offer > p {
+  font-size: 15px;
+  font-weight: 600;
+}
+
+.band-img {
+  max-width: 74rem;
+  margin-inline: auto;
+}
+
+.session-band-program-background {
+  padding-top: 400px;
+  padding-bottom: 100px;
+  margin-top: -300px;
+  max-width: 100vw;
+  margin-bottom: 100px;
+  background: #000;
+}
+
+.session-band-program {
+  color: #fff;
+  max-width: 74rem;
+  margin-inline: auto;
+  /* background: yellow; */
+}
+
+.session-band-program > h2 {
+  font-size: 40px;
+  font-family: 'F37Hybrid-Bold';
+  margin-bottom: 30px;
+}
+
+.session-band-program-container {
+  display: flex;
+  gap: 60px;
+}
+
+.article-texts {
+  font-family: 'Inter';
+  font-size: 15px;
+  line-height: 25px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+.article-texts > p {
+  max-width: 680px;
+  text-align: justify;
+}
+
+.music-camps {
+  max-width: 74rem;
+  margin-inline: auto;
+  display: flex;
+  align-items: center;
+  gap: 60px;
+  margin-bottom: 100px;
+}
+
+.music-camps > .info {
+  /* display: flex;
+  justify-content: center; */
+  color: #000;
+  max-width: 680px;
+}
+
+.music-camps > .info > h2 {
+  margin-bottom: 30px;
+  font-size: 40px;
+  font-family: 'F37Hybrid-Bold';
+}
+
+.info > p {
+  font-size: 15px;
+  text-align: justify;
+}
+
+.music-camps > .info > .btn {
+  width: fit-content;
+}
+
+.photo-container {
+  height: 660px;
+}
+
+iframe {
+  margin-top: 100px;
+}
+
+.event-festival {
+  max-width: 74rem;
+  margin-inline: auto;
+  margin-bottom: 100px;
+  margin-top: -300px;
+}
+
+.event-container {
+  display: flex;
+  align-items: center;
+  gap: 60px;
+}
+
+.right-side {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  max-width: 680px;
+}
+
+.right-side > h2 {
+  font-size: 40px;
+  font-family: 'F37Hybrid-Bold';
+}
+
+.right-side p {
+  text-align: justify;
+}
+
+.spread-the-word {
+  max-width: 74rem;
+  margin-inline: auto;
+  margin-bottom: 100px;
+}
+</style>
